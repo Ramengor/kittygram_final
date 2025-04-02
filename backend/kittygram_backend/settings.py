7df57d8a37fce51c773173aba697d6b3b2de6d8c
Вячеслав Иванов <slavik_ivanov_2015@bk.ru>
2025-04-02 21:49:27 +0300

    new three

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -7,7 +7,6 @@ from dotenv import load_dotenv
 load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 SECRET_KEY = os.getenv('SECRET_KEY', default='token')
 
 DEBUG = os.getenv("DEBUG", "False").lower() == "true"
