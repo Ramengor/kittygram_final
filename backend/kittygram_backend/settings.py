0490c973c89c26ff9c51a573f66eb95fa590a4ec
Вячеслав Иванов <slavik_ivanov_2015@bk.ru>
2025-04-04 00:48:28 +0300

    change settings

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', default='token')
 
 DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='127.0.0.1, localhost').split(', ')
+ALLOWED_HOSTS = ['89.169.169.195', '127.0.0.1', 'localhost', 'slavainfra.zapto.org']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
